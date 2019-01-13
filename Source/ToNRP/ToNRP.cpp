349634f54a0fffa8c75f69bde82f33163940d526
SirChopwood <sirchopwood@gmail.com>
2019-01-13 20:07:10 +0000

    C++ Support
    Have fun with this one Leon!

---
+++ Source/ToNRP/ToNRP.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ToNRP.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ToNRP, "ToNRP" );
